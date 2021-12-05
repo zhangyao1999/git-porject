519ee9c5a8a17b6123f66e0b6cee6d8044861cb6
zyao <imzhangyao@gmail.com>
2021-12-05 19:38:47 +0800

    hot-fix commit2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix2");
     }
 }
